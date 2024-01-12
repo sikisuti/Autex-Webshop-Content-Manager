2fe7b16fe89cf65f8b0c9e5a5f42b13077e6285c
tamas.siklosi <tamas.siklosi@westernunion.com>
2024-01-12 09:23:41 +0100

    iconize main menu

--- src/main/java/org/autex/controller/MainViewController.java
+++ src/main/java/org/autex/controller/MainViewController.java
@@ -1,16 +1,22 @@
 package org.autex.controller;
 
+import static java.util.Optional.ofNullable;
+import static javafx.scene.control.ContentDisplay.BOTTOM;
+
 import java.io.IOException;
+import java.net.URL;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.autex.App;
@@ -18,25 +24,49 @@ import org.autex.util.Configuration;
 
 public class MainViewController {
     @FXML private BorderPane rootPane;
-    @FXML private ComboBox<SupplyerDTO> cmbSupplierPicker;
+    @FXML private TilePane tpSupplierPicker;
     @FXML private Label lblMissingCredentials;
+
+  private final ObjectProperty<SupplyerDTO> selectedSupplyer = new SimpleObjectProperty<>();
 
     @FXML
     public void initialize() throws IOException {
-
-
-        ObservableList<SupplyerDTO> suppliers = FXCollections.observableArrayList(
+        var suppliers = FXCollections.observableArrayList(
                 loadFXML("File"),
                 loadFXML("Autex"),
                 loadFXML("Complex")
         );
-        cmbSupplierPicker.setItems(suppliers);
+
+        selectedSupplyer.addListener((observableValue, oldSupplier, newSupplier) -> rootPane.setCenter(newSupplier.view));
+        suppliers.forEach(supplier -> tpSupplierPicker.getChildren().add(getButton(supplier)));
         lblMissingCredentials.setVisible(Configuration.isCredentialsMissing());
     }
 
+  private Button getButton(SupplyerDTO supplyer) {
+    var button = new Button(supplyer.name);
+    var img = ofNullable(supplyer.imgPath)
+        .map(imgPath -> getClass().getResource(imgPath))
+        .map(URL::toExternalForm)
+        .map(imgPath -> {
+          var imgView = new ImageView(imgPath);
+          imgView.setFitHeight(150);
+          imgView.setFitWidth(180);
+          imgView.setPreserveRatio(true);
+          return imgView;
+        })
+        .orElse(null);
+    button.setGraphic(img);
+    button.setContentDisplay(BOTTOM);
+    button.setPrefWidth(200);
+    button.setPrefHeight(200);
+    button.setOnAction(actionEvent -> selectedSupplyer.setValue(supplyer));
+
+    return button;
+  }
+
     private SupplyerDTO loadFXML(String name) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/" + name + ".fxml"));
-        return new SupplyerDTO(name, fxmlLoader.load(), fxmlLoader.getController());
+        var fxmlLoader = new FXMLLoader(App.class.getResource("view/" + name + ".fxml"));
+        return new SupplyerDTO(name, fxmlLoader.load(), fxmlLoader.getController(), "/img/" + name + "_icon.png");
     }
 
     @FXML
@@ -48,18 +78,8 @@ public class MainViewController {
         stage.initOwner(rootPane.getScene().getWindow());
         stage.initModality(Modality.APPLICATION_MODAL);
         ResultViewController resultViewController = loader.getController();
-        resultViewController.convert(cmbSupplierPicker.getValue().controller.getConversionTask());
+        resultViewController.convert(selectedSupplyer.get().controller.getConversionTask());
         stage.show();
-    }
-
-    @FXML
-    private void updateSupplierView(ActionEvent event) {
-        rootPane.setCenter(cmbSupplierPicker.getValue().view);
-    }
-
-    @FXML
-    private void openFile() {
-
     }
 
     @FXML
@@ -78,11 +98,13 @@ public class MainViewController {
         private final String name;
         private final Node view;
         private final SupplierController controller;
+        private final String imgPath;
 
-        private SupplyerDTO(String name, Node view, SupplierController controller) {
+        private SupplyerDTO(String name, Node view, SupplierController controller, String imgPath) {
             this.name = name;
             this.view = view;
             this.controller = controller;
+            this.imgPath = imgPath;
         }
 
         @Override
